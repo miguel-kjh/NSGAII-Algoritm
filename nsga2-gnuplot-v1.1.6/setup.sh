ebe91a3414befb5a7501a26c9f2273dad7763874
miguel-kjh <miguelangel.kjh@gmail.com>
2021-01-02 16:15:16 +0000

    update bash script

--- nsga2-gnuplot-v1.1.6/setup.sh
+++ nsga2-gnuplot-v1.1.6/setup.sh
@@ -10,17 +10,19 @@ file_aux="aux.in";
 exec="nsga2r";
 random_seed=0.35;
 
+function modify_parameter_files() {
+    sed "$1" $file > $file_aux;
+    cat $file_aux > $file;
+    rm -fr $file_aux;
+}
+
 for ((idx=0; idx<${#mutation[@]}; ++idx)); do
     for ((jdx=0; jdx<${#poblation[@]}; ++jdx)); do
         folder="exp_pb_${poblation[idx]}_mut_${mutation[jdx]}";
 
-        sed "1c${poblation[idx]}" $file > $file_aux;
-        cat $file_aux > $file;
-        rm -fr $file_aux;
+        modify_parameter_files "1c${poblation[idx]}"
 
-        sed "12c${mutation[jdx]}" $file > $file_aux;
-        cat $file_aux > $file;
-        rm -fr $file_aux;
+        modify_parameter_files "12c${mutation[jdx]}"
 
         ./$exec $random_seed < $file;
 
