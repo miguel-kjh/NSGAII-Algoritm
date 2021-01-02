43a1dad0a63896d53ec6b808b4ff93a7e61053d2
miguel-kjh <miguelangel.kjh@gmail.com>
2021-01-02 16:02:27 +0000

    update experitments

--- nsga2-gnuplot-v1.1.6/setup.sh
+++ nsga2-gnuplot-v1.1.6/setup.sh
@@ -3,30 +3,32 @@
 declare -a mutation;
 declare -a poblation;
 
-mutation=('0.2' '0.3' '0.4' '0.5' '0.1');
-poblation=('100' '100' '200' '300' '1000');
+mutation=('0.1' '0.3' '0.4');
+poblation=('100' '500' '1000');
 file="fon.in";
 file_aux="aux.in";
 exec="nsga2r";
-random=0.35;
+random_seed=0.35;
 
 for ((idx=0; idx<${#mutation[@]}; ++idx)); do
-    folder="exp_pb_${poblation[idx]}_mut_${mutation[idx]}";
+    for ((jdx=0; jdx<${#poblation[@]}; ++jdx)); do
+        folder="exp_pb_${poblation[idx]}_mut_${mutation[jdx]}";
 
-    sed "1c${poblation[idx]}" $file > $file_aux;
-    cat $file_aux > $file;
-    rm -fr $file_aux;
+        sed "1c${poblation[idx]}" $file > $file_aux;
+        cat $file_aux > $file;
+        rm -fr $file_aux;
 
-    sed "12c${mutation[idx]}" $file > $file_aux;
-    cat $file_aux > $file;
-    rm -fr $file_aux;
+        sed "12c${mutation[jdx]}" $file > $file_aux;
+        cat $file_aux > $file;
+        rm -fr $file_aux;
 
-    ./$exec 0.35 < $file;
+        ./$exec $random_seed < $file;
 
-    rm -fr $folder;
-    mkdir $folder;
+        rm -fr $folder;
+        mkdir $folder;
 
-    mv *.out -t $folder;
+        mv *.out -t $folder;
+    done
 done
 
 exit 0;
