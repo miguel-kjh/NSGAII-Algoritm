b7f96bd81ce027dc6dea73ea89d745a728966425
miguel-kjh <miguelangel.kjh@gmail.com>
2021-01-03 12:02:40 +0000

    added 0.02 rate

--- nsga2-gnuplot-v1.1.6/setup.sh
+++ nsga2-gnuplot-v1.1.6/setup.sh
@@ -3,7 +3,7 @@
 declare -a mutation;
 declare -a population;
 
-mutation=('0.1' '0.3' '0.4');
+mutation=('0.02' '0.1' '0.3' '0.4');
 population=('100' '500' '1000');
 file="fon.in";
 file_aux="aux.in";
@@ -18,11 +18,11 @@ function modify_parameter_files() {
 
 for ((idx=0; idx<${#mutation[@]}; ++idx)); do
     for ((jdx=0; jdx<${#population[@]}; ++jdx)); do
-        folder="exp_pb_${population[idx]}_mut_${mutation[jdx]}";
+        folder="exp_pb_${population[jdx]}_mut_${mutation[idx]}";
 
-        modify_parameter_files "1c${population[idx]}"
+        modify_parameter_files "1c${population[jdx]}"
 
-        modify_parameter_files "12c${mutation[jdx]}"
+        modify_parameter_files "12c${mutation[idx]}"
 
         ./$exec $random_seed < $file;
 
